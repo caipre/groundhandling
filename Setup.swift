130b021bad90e0a9a9962f6070a458f8fc01f14c
Nick Platt <platt.nicholas@gmail.com>
2020-08-31 02:06:34 -0400

    build: add tooling to generate license files

--- Setup.swift
+++ Setup.swift
@@ -1,6 +1,11 @@
 import ProjectDescription
 
 let setup = Setup([
-  .homebrew(packages: ["swift-format", "swiftlint"]),
-  .custom(name: "git hooks", meet: ["./hooks/install.sh"], isMet: ["./hooks/install.sh", "check"]),
+  .homebrew(packages: ["jq", "swift-format", "swiftlint"]),
+  .custom(
+    name: "git hooks",
+    meet: ["./scripts/install-hooks.sh"],
+    isMet: ["./scripts/install-hooks.sh", "check"]
+  ),
+  .custom(name: "rq", meet: ["./scripts/install-rq.sh"], isMet: ["which", "rq"]),
 ])
---
+++ scripts/generate-licenses-json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for yml in App/Resources/Licenses/*.yaml ; do rq <$yml -yJ ; done | jq --slurp . >App/Resources/Licenses/licenses.json
