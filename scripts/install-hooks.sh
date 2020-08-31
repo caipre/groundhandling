507510b9e16a3f11e20f7b978d759c5ad968f0a0
Nick Platt <platt.nicholas@gmail.com>
2020-08-31 02:06:34 -0400

    build: add tooling to generate license files

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 for hook in hooks/*; do
-   if [[ $hook == hooks/install.sh ]]; then continue; fi
    if [[ $@ == check ]]; then
       diff $hook .git/$hook &>/dev/null || exit 1
    else
---
+++ scripts/install-rq.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -LSfs https://japaric.github.io/trust/install.sh | sh -s -- --git dflemstr/rq
